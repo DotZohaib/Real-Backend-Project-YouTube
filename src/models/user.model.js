21b370452a1824648a714582e457cd932abb8849
dotzohaib <dayozohaibali@gmail.com>
2026-03-01 16:26:23 +0500

    final

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-const userModel = mongoose.Schema({
+const userModel = new mongoose.Schema({
   username: {
     type: String,
     required: true,
